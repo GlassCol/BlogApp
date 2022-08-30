d87024622e6f522b2e8b558cfa52ffdedf4c0d75
kujo571 <alexthomas5340@gmail.com>
2022-08-30 08:26:03 -0400

    created boilerplate for frontend components

---
+++ frontend/src/components/CreateAnAccount.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateAnAccount = () => {
+  return (
+    <h1>
+        Create An Account Page
+    </h1>
+  )
+}
+
+export default CreateAnAccount
---
+++ frontend/src/components/postsAndReplies.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostsAndReplies = () => {
+  return (
+      <h1>Post and Replies Page</h1>
+        )
+}
+
+export default PostsAndReplies
