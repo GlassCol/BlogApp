9ce6ef72df148bfb991e3d4df578f7bf71da4a26
kujo571 <alexthomas5340@gmail.com>
2022-08-31 14:37:17 -0400

    login funcitions

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,7 +3,19 @@ import React from 'react'
 const Login = () => {
   return (
     <form>
-        
+        <label for="username-txt">
+            <b>Username:</b>
+        </label> 
+        <br/>
+        <input type="text" name="username-txt" id="username-txt" placeholder="Enter Username:" pattern="[A-Za-zd.]{4,8}" required></input>
+        <br/>
+        <label for="password-txt">
+            <b>Password:</b>
+        </label>
+        <br/>
+        <input type="text" name="password-txt" id="password-txt" placeholder="Enter Password:" required></input>
+        <br/><br/>
+        <button id="submit-btn" onClick="">submit</button>
     </form>
   )
 }
